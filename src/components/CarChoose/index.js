ad7034a48f2d7eda40f9291a3a9275bb6044f65b
benweizhu <benweizhu@gmail.com>
2017-08-05 11:58:05 +0800

    add carchoose basic component

--- src/components/CarChoose/index.js
+++ src/components/CarChoose/index.js
@@ -1,23 +1,61 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from "react-flexbox-grid";
 import { connect } from "react-redux";
+import { RadioGroup, Radio } from "react-radio-group";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "./CarChoose.css";
 
 const mapStateToProps = state => {
+  const carTypes = ["奔驰", "宝马"];
   return {
-    carType: state.carType 
+    carType: state.carType,
+    carTypes
   };
 };
 
 class CarChoose extends Component {
   constructor(props) {
     super(props);
+    this.state = { carType: "奔驰" };
   }
 
+  handleChange = value => {
+    this.setState({ carType: value });
+  };
+
   render() {
-    return <div>{this.props.carType}</div>;
+    return (
+      <div className="car-choose">
+        <div>
+          <RadioGroup
+            name="carType"
+            selectedValue={this.state.carType}
+            onChange={this.handleChange}
+          >
+            <ul>
+              {this.props.carTypes.map(carType =>
+                <li>
+                  <label>
+                    <Radio value={carType} />
+                    {carType}
+                  </label>
+                </li>
+              )}
+            </ul>
+          </RadioGroup>
+        </div>
+        <Grid fluid>
+          <Row>
+            <Col xs={12}>
+              <Link to="/">
+                <button>确定选择</button>
+              </Link>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
   }
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Grid, Row, Col } from "react-flexbox-grid";
 import AssessedValueCard from "../AssessedValueCard";
 import { connect } from "react-redux";
-
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "./Home.css";
