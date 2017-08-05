794e624da5af07f14f823b6039063d1363d1a9a4
benweizhu <benweizhu@gmail.com>
2017-08-05 12:36:28 +0800

    car choose basic style

--- src/components/CarChoose/index.js
+++ src/components/CarChoose/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from "react-flexbox-grid";
+import classnames from "classnames";
 import { connect } from "react-redux";
 import { RadioGroup, Radio } from "react-radio-group";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
@@ -27,27 +28,33 @@ class CarChoose extends Component {
   render() {
     return (
       <div className="car-choose">
-        <div>
-          <RadioGroup
-            name="carType"
-            selectedValue={this.state.carType}
-            onChange={this.handleChange}
-          >
-            <ul>
-              {this.props.carTypes.map(carType =>
-                <li>
-                  <label>
-                    <Radio value={carType} />
-                    {carType}
-                  </label>
-                </li>
-              )}
-            </ul>
-          </RadioGroup>
-        </div>
         <Grid fluid>
           <Row>
             <Col xs={12}>
+              <RadioGroup
+                name="carType"
+                selectedValue={this.state.carType}
+                onChange={this.handleChange}
+              >
+                <ul>
+                  {this.props.carTypes.map(carType =>
+                    <li>
+                      <label
+                        className={classnames({
+                          active: this.state.carType === carType
+                        })}
+                      >
+                        <Radio value={carType} />
+                        {carType}
+                      </label>
+                    </li>
+                  )}
+                </ul>
+              </RadioGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} className="choose-button">
               <Link to="/">
                 <button>确定选择</button>
               </Link>
