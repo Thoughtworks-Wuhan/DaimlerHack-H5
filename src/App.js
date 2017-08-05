ddc011e6d4de642cb1db031886e739452600946b
benweizhu <benweizhu@gmail.com>
2017-08-05 10:32:43 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,66 +1,23 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from "react-flexbox-grid";
-import AssessedValueCard from "./components/AssessedValueCard";
+import Home from "./components/Home";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      assessedValue: -1
-    };
   }
 
-  handleAssess = () => {
-    this.setState({
-      assessedValue: 50000
-    });
-  };
   render() {
     return (
-      <div className="app">
-        <Grid fluid>
-          <Row className="hack-row">
-            <Col xs={12}>
-              <h1>二手车估值</h1>
-            </Col>
-          </Row>
-          <Row className="hack-row">
-            <Col xs={4}>
-              <label htmlFor="">车型选择</label>
-            </Col>
-            <Col xs={8}>
-              <input type="text" />
-            </Col>
-          </Row>
-          <Row className="hack-row">
-            <Col xs={4}>
-              <label htmlFor="">车型选择</label>
-            </Col>
-            <Col xs={8}>
-              <input type="text" />
-            </Col>
-          </Row>
-          <Row className="hack-row">
-            <Col xs={4}>
-              <label htmlFor="">车型选择</label>
-            </Col>
-            <Col xs={8}>
-              <input type="text" />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <button onClick={this.handleAssess}>估值</button>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <AssessedValueCard assessedValue={this.state.assessedValue} />
-            </Col>
-          </Row>
-        </Grid>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { Grid, Row, Col } from "react-flexbox-grid";
+import AssessedValueCard from "../AssessedValueCard";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "./Home.css";
+import "font-awesome/css/font-awesome.css";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      assessedValue: -1
+    };
+  }
+
+  handleAssess = () => {
+    this.setState({
+      assessedValue: 50000
+    });
+  };
+
+  render() {
+    return (
+      <div className="home">
+        <Grid fluid>
+          <Row className="hack-row">
+            <Col xs={12}>
+              <h1>二手车估值</h1>
+            </Col>
+          </Row>
+          <Row className="hack-row">
+            <Col xs={4}>
+              <label htmlFor="">车型选择</label>
+            </Col>
+            <Col xs={8}>
+              <div className="hack-select-button">
+                <i className="fa fa-angle-right" aria-hidden="true" />
+              </div>
+            </Col>
+          </Row>
+          <Row className="hack-row">
+            <Col xs={4}>
+              <label htmlFor="">车型选择</label>
+            </Col>
+            <Col xs={8}>
+              <input className="text-input" type="text" />
+            </Col>
+          </Row>
+          <Row className="hack-row">
+            <Col xs={4}>
+              <label htmlFor="">车型选择</label>
+            </Col>
+            <Col xs={8}>
+              <input className="text-input" type="text" />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <button onClick={this.handleAssess}>估值</button>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <AssessedValueCard assessedValue={this.state.assessedValue} />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Home;
