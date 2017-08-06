091558aa3fb921d0e76349281eb70821bff62458
benweizhu <benweizhu@gmail.com>
2017-08-06 14:18:27 +0800

    add data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,10 @@ const defaultState = {
   carType: "奔驰GLA级 2015款 GLA 200 时尚型",
   cars: [
     { title: "奔驰GLA级 2015款 GLA 200 时尚型" },
-    { title: "马自达CX-5 2015款 2.0L 手动两驱舒适型" }
+    { title: "马自达CX-5 2015款 2.0L 手动两驱舒适型" },
+    { title: "本田XR-V 2015款 1.8L VTi CVT豪华版" },
+    { title: "奥迪Q5 2013款 40 TFSI 技术型" },
+    { title: "保时捷Macan 2014款 Macan 2.0T" }
   ],
   year: "",
   month: "",
