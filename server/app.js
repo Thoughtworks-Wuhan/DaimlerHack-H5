34bb8cf1410d0a2297d91cf31e74a625bd9938c8
benweizhu <benweizhu@gmail.com>
2017-08-07 21:37:45 +0800

    add proxy server

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require('cors');
+const estimate = require("./estimate");
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.use(cors());
+
+app.post("/estimate", function(req, res) {
+  estimate(req.body)
+  return 'hello world';
+});
+
+app.listen(5555, function() {
+  console.log("Example app listening on port 5555!");
+});
---
+++ server/estimate.js
@@ -0,0 +1,99 @@
+var request = require("request");
+var url = require("url");
+var crypto = require("crypto");
+
+// 这里填写AK和请求
+const md5 = function(buffer) {
+  var hash;
+  hash = crypto.createHash("md5");
+  hash.update(buffer);
+  return hash.digest("base64");
+};
+
+const sha1 = function(stringToSign, secret) {
+  var signature;
+  return (signature = crypto
+    .createHmac("sha1", secret)
+    .update(stringToSign)
+    .digest()
+    .toString("base64"));
+};
+
+module.exports = body => {
+  var date = new Date().toUTCString();
+  // 这里填写AK和请求
+  var ak_id = "LTAIVNsDfkqFBhG9";
+  var ak_secret = "oYK90eEKXbWzq4TxZgWsJ8kPgGakUQ";
+  var options = {
+    url:
+      "https://dtplus-cn-shanghai.data.aliyuncs.com/dt_ng_1747801636299161/pai/prediction/projects/daimler_hack/onlinemodels/xlab_m_linearregressio_684991_v1",
+    method: "POST",
+    body: JSON.stringify({
+      inputs: [
+        {
+          roal_haul: {
+            dataType: 40,
+            dataValue: 0.1036
+          },
+          new_price: {
+            dataType: 40,
+            dataValue: 0.102
+          },
+          use_date: {
+            dataType: 40,
+            dataValue: 0.254
+          },
+          insurance_date: {
+            dataType: 40,
+            dataValue: 0.198
+          }
+        }
+      ]
+    }),
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      date: date,
+      Authorization: ""
+    }
+  };
+
+  // step1: 组stringToSign [StringToSign = #{method}\\n#{accept}\\n#{data}\\n#{contentType}\\n#{date}\\n#{action}]
+  var body = options.body || "";
+  var bodymd5;
+  if (body === void 0 || body === "") {
+    bodymd5 = body;
+  } else {
+    bodymd5 = md5(new Buffer(body));
+  }
+  console.log(bodymd5);
+  var stringToSign =
+    options.method +
+    "\n" +
+    options.headers.accept +
+    "\n" +
+    bodymd5 +
+    "\n" +
+    options.headers["content-type"] +
+    "\n" +
+    options.headers.date +
+    "\n" +
+    url.parse(options.url).path;
+  console.log("step1-Sign string:", stringToSign);
+  // step2: 加密 [Signature = Base64( HMAC-SHA1( AccessSecret, UTF-8-Encoding-Of(StringToSign) ) )]
+  var signature = sha1(stringToSign, ak_secret);
+  // console.log("step2-signature:", signature);
+  // step3: 组authorization header [Authorization =  Dataplus AccessKeyId + ":" + Signature]
+  var authHeader = "Dataplus " + ak_id + ":" + signature;
+  console.log("step3-authorization Header:", authHeader);
+  options.headers.Authorization = authHeader;
+  console.log("authHeader", authHeader);
+  // step4: send request
+  function callback(error, response, body) {
+    if (error) {
+      console.log("error", error);
+    }
+    console.log("step4-response body:", response.statusCode, body);
+  }
+  request(options, callback);
+};
